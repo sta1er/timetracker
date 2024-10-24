3644fa365dbb76953e2aa255ae875fce73d3ecf8
sta1er <vlad.sokogubov@gmail.com>
2024-10-24 19:09:02 +0300

    правки

--- src/main/java/com/example/timetracker/config/security/SecurityConfig.java
+++ src/main/java/com/example/timetracker/config/security/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
